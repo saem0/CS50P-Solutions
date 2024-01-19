7a30c801baf0c34bdcdc17d7e5a593caf8a972f7
Abid Hasan Saem <157108298+saem0@users.noreply.github.com>
2024-01-19 20:54:29 +0600

    Update deep.py

--- Week 1/deep.py
+++ Week 1/deep.py
@@ -1,1 +1,7 @@
+message = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
+
+if message == "42" or message == "forty-two" or message == "forty two":
+    print("Yes")
+else:
+    print("No")
 
