a772f13e80979f1c68a005634a3fcc8c7b39a682
Abid Hasan Saem <157108298+saem0@users.noreply.github.com>
2024-01-20 15:10:57 +0600

    Update camel.py

--- Week 2/camel.py
+++ Week 2/camel.py
@@ -1,1 +1,12 @@
+camel = input("camelCase: ")
+
+print("snake_case: ", end="")
+
+
+for letter in camel:
+    if letter.isupper():
+        print("_" + letter.lower(), end="")
+    else:
+        print(letter, end="")
+print()
 
